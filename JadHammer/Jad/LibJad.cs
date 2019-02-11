b012d194165fe2b5574718327aeb3bb2665a644b
unknown <matt@RBO.LOCAL>
2019-02-11 22:01:37 +0000

    JadHammer initial commit - many many problems

---
+++ JadHammer/Jad/LibJad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Jad
+{
+	public static class LibJad
+	{
+		private const string dllName = "jad.lib";
+
+		/// <summary>
+		/// performs necessary static initialization
+		/// </summary>
+		/// <returns></returns>
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int jadStaticInit();
+
+		/// <summary>
+		/// opens a jadContext, which gets its data from the provided stream (containing a jad/jac file) and allocator
+		/// </summary>
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int jadOpen(ref JadContext jad, ref JadStream stream, ref JadAllocator allocator);
+
+		/// <summary>
+		/// creates a jadContext based on the provided jadCreationParams, which describe the disc
+		/// </summary>
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int jadCreate(ref JadContext jad, ref JadCreationParams jcp, ref JadAllocator allocator);
+
+		/// <summary>
+		/// dumps the given jad to the output stream as a JAD or JAC file
+		/// </summary>
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int jadDump(ref JadContext jad, ref JadStream stream, int JACIT);
+
+		/// <summary>
+		/// closes a jadContext opened with jadOpen or jadCreate. do not call on unopened jadContexts
+		/// </summary>
+		[DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int jadClose(ref JadContext jad);
+	}
+}
+
---
+++ JadHammer/Jad/Structs.cs
@@ -0,0 +1,104 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Jad
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadStream
+	{
+		public JadStreamRead read;
+		public JadStreamWrite write;
+		public JadStreamSeek seek;
+		public JadStreamGet get;
+		public JadStreamPut put;
+		public JadStreamFlush flush;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadAllocator
+	{
+		public JadAllocatorAlloc alloc;
+		public JadAllocatorRealloc realloc;
+		public JadAllocatorFree free;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadTocHeader
+	{
+		public byte firstTrack;
+		public byte lastTrack;
+		public byte flags;
+		public byte reserved;
+	}
+
+	/// <summary>
+	/// a timestamp like the format stored on a disc
+	/// </summary>
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadTimestamp
+	{
+		public byte MIN;
+		public byte SEC;
+		public byte FRAC;
+		public byte _padding;
+	}
+
+	[StructLayout(LayoutKind.Explicit)]
+	public struct JadSector
+	{
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 2352), FieldOffset(0)]
+		public byte[] data;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 96), FieldOffset(2352)]
+		public byte[] subcode;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 2448), FieldOffset(0)]
+		public byte[] entire;
+	}
+
+	/// <summary>
+	/// this layout does not reflect the layout of a subQ on disc; separate serialization functions are used to represent that
+	/// </summary>
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadSubchannelQ
+	{
+		public JadTimestamp q_timestamp;
+		public JadTimestamp q_apTimestamp;
+		public ushort q_crc;
+		public byte q_tno;
+		public byte q_index;
+		public byte zero;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+		public byte[] padding;
+	}
+
+	/// <summary>
+	/// TOC
+	/// </summary>
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadTOC
+	{
+		public JadTocHeader header;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 101)]
+		public JadSubchannelQ[] entries;
+	}
+
+	/// <summary>
+	/// the params struct used for jadCreate
+	/// </summary>
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadCreationParams
+	{
+		public JadTOC toc;
+		public JadAllocator allocator;
+		public int numSectors;
+		public JadCreateReadCallback callback;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct JadContext
+	{
+		public JadStream stream;
+		public JadAllocator allocator;
+		public uint flags;
+		public uint numSectors;
+		public JadCreationParams createParams;
+	}
+}
